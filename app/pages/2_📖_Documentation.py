cdb673e849cadf64db9bb723009244f24071d60c
Daniele Castellana <45374794+castledan@users.noreply.github.com>
2023-03-14 14:36:39 +0100

    Initial commit

---
+++ app/pages/2_📖_Documentation.py
@@ -0,0 +1,34 @@
+"""Documentation page for Streamlit app."""
+import streamlit as st
+from src.config_parameters import params
+from src.utils import (
+    add_about,
+    add_logo,
+    set_doc_page_style,
+    toggle_menu_button,
+)
+
+# Page configuration
+st.set_page_config(layout="wide", page_title=params["browser_title"])
+
+# If app is deployed hide menu button
+toggle_menu_button()
+
+# Create sidebar
+add_logo("app/img/MA-logo.png")
+add_about()
+
+# Set page style
+set_doc_page_style()
+
+# Page title
+st.markdown("# Documentation")
+
+# Methodology section
+st.markdown("## Methodology")
+
+# Limitations section
+st.markdown("## Key limitations")
+
+# Links section
+st.markdown("## Useful links")
