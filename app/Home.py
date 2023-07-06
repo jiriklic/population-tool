380d4d7f34fafb16eaf8cc00e155218e83fe1aa7
castledan <45374794+castledan@users.noreply.github.com>
2023-07-06 12:49:42 +0200

    add documentation

--- app/Home.py
+++ app/Home.py
@@ -34,10 +34,124 @@ st.markdown("# Home")
 st.markdown("## Introduction")
 st.markdown(
     """
-    This web app allows you to upload shapefiles and retrieve population data
-    from WorldPop.
+    <p align="justify">
+        This tool enables users to extract population information for specific
+        areas defined by shapefiles. This feature allows for precise and
+        localized analysis of population dynamics, which is particularly useful
+        for researchers and decision-makers who require population data at a
+        subnational or even sub-regional level.<br><br>
+        The population information is retrieved from <a href='%s'>WorldPop</a>,
+        an open-access data repository that focuses on generating and providing
+        detailed population data for different regions and countries around the
+        world. The aim of WorldPop is to improve our understanding of human
+        population dynamics and their interactions with social, economic, and
+        environmental factors.<br><br>
+        The organization utilizes various sources of data, including census
+        records, satellite imagery, household surveys, and other demographic
+        information, to estimate population distribution and demographic
+        characteristics at high spatial resolutions. WorldPop employs advanced
+        spatial modeling techniques and statistical methods to generate
+        population estimates at fine-scale resolutions (100 meters).<br><br>
+        It is important to remember that the accuracy of the results provided
+        by the tool may vary depending on the region due to limitations of the
+        WorldPop's population data, in terms of spatial and temporal
+        resolution, as well as inherent uncertainty resulting from modeling and
+        statistical techniques used for estimation.
+    </p>
 """
+    % (config["url_worldpop"],),
+    unsafe_allow_html=True,
 )
 
 # Second section
 st.markdown("## How to use the tool")
+st.markdown(
+    """
+    <p align="justify">
+        <b>Important.</b> The duration of each step in the tool may vary
+        based on the size of the uploaded data, and while the tool is
+        processing, you will observe a "running" status with a runner icon
+        in the top right of the screen. Please do not initiate any
+        action or proceed to the next step until the current step is
+        finished.<br>
+    </p>
+    <ul>
+        <li><p align="justify">
+            In the sidebar, choose <i>Population analysis</i> to start the
+            analysis.
+        </p>
+        <li><p align="justify">
+            Upload a shapefile (in .zip format) containing the areas for which
+            you want to extract population information. The shapefile will be
+            visualised as table underneath.
+        </p>
+        <li><p align="justify">
+            Select the preferred type of population dataset. The options are
+            <i>uncontrained</i> and <i>UNadj_contrained</i> (constrained -
+            UN adjusted). For a description of these data types, please read
+            the documentation.
+        </p>
+        <li><p align="justify">
+            Select the preferred data aggregation. You can retrieve the total
+            population for each area of interest, or disaggregated population
+            data (by gender and age). Depending on the data type, one of the
+            two options might not be available.
+        </p>
+        <li><p align="justify">
+            Specify the year.  Total population data is available for the years
+            ranging from 2000 to 2020, while disaggregated data is only
+            available for the year 2020.
+        </p>
+        <li><p align="justify">
+            If all information is filled in correctly, you will be able to
+            click on the button <i>Read to run?</i>, to start the computation.
+        </p>
+        <li><p align="justify">
+            Depending on the size of the uploaded shapefile, and on the
+            complexity of the associated geometries, different methods to
+            retrieve population data will be used, which results in different
+            computation times. For small shapefiles (usually below 10MB), the
+            computation is expected to last a few minutes, while for big
+            shapefiles, the process can last up to several hours. It is
+            important that you do not close the window, nor make any change
+            until the process is completed.
+        </p>
+        <li><p align="justify">
+            Once the computation is complete, the shapefile, complemented with
+            population information, will be visualised.
+        </p>
+        <li><p align="justify">
+            It is possible to download the output in shapefile format or csv
+            format.
+        </p>
+        <li><p align="justify">
+            Select the shapefile field you want to use to assign labels to the
+            population data in the plot.
+        </p>
+        <li><p align="justify">
+            Create the plot, by clicking on the button.
+        </p>
+        <li><p align="justify">
+            The interactive plot shows population information for each area
+            of interest. If disaggregated data was selected, a population
+            pyramid will be created for each area.
+        </p>
+        <li><p align="justify">
+            By clicking on <i>Download figure(s)</i>, you can download the
+            plots. The plots are saved as .png figures. If disaggregated data
+            was selected, a .zip file is created, containing one figure for
+            each area of interest.
+        </p>
+        <li><p align="justify">
+            You can restart the analysis by clicking on the correponding
+            button.
+        </p>
+    </ul>
+    <p align="justify">
+        In case you get errors, follow the intructions, or try to refresh the
+        web page. If you have doubts, feel free to contact the Data Science
+        team.
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
--- app/pages/2_📖_Documentation.py
+++ app/pages/2_📖_Documentation.py
@@ -1,11 +1,20 @@
 """Documentation page for Streamlit app."""
+import os
+
 import streamlit as st
+import yaml
 from src.utils import (
     add_about,
     add_logo,
     set_doc_page_style,
     toggle_menu_button,
 )
+
+# Load configuration options
+config_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, "config", "config.yml")
+)
+config = yaml.safe_load(open(config_path))
 
 # Page configuration
 # st.set_page_config(layout="wide", page_title=config["browser_title"])
@@ -25,9 +34,140 @@ st.markdown("# Documentation")
 
 # Methodology section
 st.markdown("## Methodology")
+st.markdown(
+    """
+    <h4>Worldpop data</h4>
+    <p align="justify">
+        WorldPop is a renowned research initiative and open-access data
+        repository that plays a pivotal role in providing comprehensive and
+        detailed population data for regions and countries around the world.
+        Their extensive datasets offer valuable insights into population
+        dynamics and their interactions with various socio-economic and
+        environmental factors.<br><br>
+        Available datasets encompass both population totals and disaggregated
+        information, enabling researchers and practitioners to analyze
+        population patterns at different scales. The population totals dataset
+        provides an overview of the overall population count for specific
+        regions, while the disaggregated dataset offers a more nuanced
+        understanding of demographic characteristics such as age and
+        gender.<br><br>
+        WorldPop offers population data at varying resolutions, including 1km
+        and 100m. The tool utilizes the 100m data, as it provides a higher
+        level of accuracy when estimating population for areas of interest,
+        regardless of their size.<br><br>
+        WorldPop's population data includes different data types. The
+        <b>unconstrained</b> dataset provides estimates based on various data
+        sources and modeling techniques, while the constrained dataset
+        incorporates additional constraints, such as known total population or
+        administrative data, to improve the accuracy of the estimates.
+        Furthermore, WorldPop provides UN adjusted datasets, which align with
+        the United Nations' demographic estimates and projections.<br><br>
+        The availability of data varies across different years. The total
+        population data spans from the year 2000 to 2020, enabling researchers
+        to analyze population trends and changes over a significant time
+        period. On the other hand, the disaggregated data, which provides more
+        detailed information, is currently limited to the year 2020.<br><br>
+        For more information, visit <a href='%s'>their website</a>.
+    </p>
+    <h4>WorldPop and Google Earth Engine</h4>
+    <p align="justify">
+        WorldPop's population data is accessible through their API, with the
+        option to download TIF files per country. However, a challenge arises
+        when dealing with areas of interest that span multiple countries or
+        represent only small proportions of those countries. Downloading TIF
+        files for entire countries can be inconvenient and time-consuming,
+        potentially causing memory issues.<br><br>
+        Thankfully, alternative methods for data retrieval exist. Google Earth
+        Engine (GEE) offers <a href='%s'>access</a> to the most commonly used
+        WorldPop datasets. With GEE, it becomes possible to create a mosaic of
+        population data, clip the raster according to the specific areas of
+        interest, and then calculate zonal statistics. This approach allows for
+        a more targeted extraction of population information without the need
+        to download complete country-level TIF files.<br><br>
+        For most shapefiles, the tool efficiently conducts all the computations
+        on the server side of Google Earth Engine (GEE). This means that the
+        heavy computational tasks, such as processing and analyzing the
+        population data, are performed within the GEE infrastructure. As a
+        result, users can avoid the need to download the entire dataset and
+        instead retrieve only the final population figures, optimizing both
+        time and storage resources.<br><br>
+        Unfortunately, GEE has limitations for the size of the data that can
+        be uploaded (usually restricted to 10 MB). To overcome such
+        limitations, a workaround is required. The solution involves creating
+        a bounding box that encompasses the geometries of interest, importing
+        it into GEE, clipping the raster data within GEE, downloading the
+        resulting TIF file <a href='%s'>using <i>geemap</i></a>, and performing
+        the calculation of zonal statistics locally.
+    </p>
+    <h4>Zonal statistics</h4>
+    <p align="justify">
+        When all computations are performed within GEE, the calculation of
+        zonal statistics is seamlessly integrated into the GEE workflow. GEE
+        provides built-in methods and functions specifically designed for zonal
+        statistics, allowing users to extract statistical information, such as
+        mean, sum, or maximum, directly within the GEE environment. In
+        cases where large shapefiles are involved, the zonal statistics
+        computation are performed in Python. The library <i>rasterstats</i>
+        offers a useful function called <i>zonal_stats</i> that enables the
+        calculation of zonal statistics outside of the GEE environment.
+    </p>
+    """
+    % (
+        config["url_worldpop"],
+        config["url_worldpop_GEE"],
+        config["url_geemap_download"],
+    ),
+    unsafe_allow_html=True,
+)
 
 # Limitation section
 st.markdown("## Key limitations")
+st.markdown(
+    """
+    <h4>Worldpop data</h4>
+    <ul>
+        <li><p align="justify">
+            Spatial and temportal resolution may vary, depending on the region
+            and data source. In some cases, the data may not capture very
+            small-scale population variations or recent changes due to the lag
+            in data availability or limitations in the input data sources.
+        </p>
+        <li><p align="justify">
+            The accuracy of WorldPop's population estimates may be influenced
+            by the inherent uncertainty associated with the modeling and
+            statistical techniques used for estimation.
+        </p>
+        <li><p align="justify">
+            Data availability may differ across regions and years, with some
+            areas having more comprehensive data coverage than others.
+        </p>
+        <li><p align="justify">
+            The disaggregated data is currently limited to the year 2020,
+            restricting the ability to analyze population dynamics over an
+            extended time period.
+        </p>
+    </ul>
+    <h4>Zonal statistics algorithms</h4>
+    <ul>
+        <li><p align="justify">
+            The algorithms may have limitations in handling
+            irregular or complex polygon geometries, leading to potential
+            inaccuracies in the calculated statistics.
+        </p>
+        <li><p align="justify">
+            The algorithms may struggle with edge effects, where pixel values
+            near the boundary of a zone may be influenced by neighboring zones,
+            affecting the accuracy of the statistics.
+        </p>
+        <li><p align="justify">
+            If the resolution of the raster data is significantly different
+            from the polygon boundaries, errors or biases may occur due to
+            pixel aggregation or disaggregation.
+        </p>
+    </ul>
+    """,
+    unsafe_allow_html=True,
+)
 
 # Link section
-st.markdown("## Useful links")
+# st.markdown("## Useful links")
